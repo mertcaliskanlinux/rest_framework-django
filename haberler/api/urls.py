407c0735cc3c4c3ec5535f5479c6d38f7bf75c74
mertcaliskanlnx <mertcaliskanlinux@gmail.com>
2022-12-11 02:01:55 +0300

    url'ler Test Edildi

--- haberler/api/urls.py
+++ haberler/api/urls.py
@@ -3,7 +3,7 @@ from haberler.api import views as api_views
 
 urlpatterns = [
     path('yazarlar/',api_views.GazeteciListCreateAPIView.as_view(),name='yazarlar-listesi'),
-    path('makaleler/>',api_views.MakaleListCreateAPIView.as_view(),name='makale-listesi'),
+    path('makaleler/',api_views.MakaleListCreateAPIView.as_view(),name='makale-listesi'),
     path('makaleler/<int:pk>',api_views.MakaleDetailAPİView.as_view(),name='makale-detay'),
 ]
 
