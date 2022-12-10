d2438f056cd7a57a6bff9ed7e263d9af9df2f5f7
mertcaliskanlnx <mertcaliskanlinux@gmail.com>
2022-12-11 02:41:48 +0300

    kitaplar app

--- kitaplar/models.py
+++ kitaplar/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
-# Create your models here.
+class Kitap(models.Model):
+    isim = models.CharField(max_length=255)
+    yazar = models.CharField(max_length=255)
+    aciklama = models.TextField()
+    
+    yaratilma_tarihi = models.DateTimeField(auto_now_add=True)
+    guncelleme_tarihi = models.DateTimeField(auto_now_add=True)  
+    yayın_tarihi = models.DateTimeField()
+    
+    def __str__(self):
+        return f"{self.isim} - {self.yazar}"
+
+class Yorum(models.Model):
+    kitap = models.ForeignKey(Kitap, related_name='yorumlar', on_delete=models.CASCADE)
+    
+    yorum_sahibi = models.CharField(max_length=255)
+    yorum = models.TextField(blank=True,null=True)
+    
+    yaratilma_tarihi = models.DateTimeField(auto_now_add=True)
+    guncelleme_tarihi = models.DateTimeField(auto_now_add=True)  
+
+    degerlendirme = models.PositiveIntegerField(
+        validators=[MinValueValidator(1),MaxValueValidator(5)]
+    )
+    
+    def __str__(self):
+        return str(self.degerlendirme)
+    
