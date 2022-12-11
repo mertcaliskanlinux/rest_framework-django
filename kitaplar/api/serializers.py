5dbe6462f5f366163eba05e3fcb7becd39f2e2d2
mertcaliskanlnx <mertcaliskanlinux@gmail.com>
2022-12-11 03:31:40 +0300

    kitap-yorum serializers yapıldı

---
+++ kitaplar/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from kitaplar.models import Kitap,Yorum
+
+    
+class YorumSerializers(serializers.ModelSerializer):
+    
+    
+    class Meta:
+        model = Yorum
+        fields = '__all__'
+        
+        
+class KitapSerializers(serializers.ModelSerializer):
+    yorumlar = YorumSerializers(many=True,read_only=True)
+    
+    
+    class Meta:
+        model = Kitap
+        fields = '__all__'
+    
+    
+    
+    
