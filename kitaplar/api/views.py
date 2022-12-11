a7a1bca2ac3b8edcc935d5f2f9a9feceeb05a9e0
mertcaliskanlnx <mertcaliskanlinux@gmail.com>
2022-12-11 04:28:10 +0300

    yorumlar

--- kitaplar/api/views.py
+++ kitaplar/api/views.py
@@ -1,9 +1,11 @@
 # from rest_framework.generics import GenericAPIView
 # from rest_framework.mixins import ListModelMixin,CreateModelMixin
 
-from kitaplar.api.serializers import KitapSerializers
-from kitaplar.models import Kitap
+from kitaplar.api.serializers import KitapSerializers,YorumSerializers
+from kitaplar.models import Kitap,Yorum
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+
 
 
 
@@ -18,7 +20,27 @@ class KitapDetailAPIView(generics.RetrieveDestroyAPIView):
     serializer_class = KitapSerializers
 
 
-
+class YorumCreateAPIView(generics.CreateAPIView):
+    queryset = Yorum.objects.all()
+    serializer_class = YorumSerializers
+    
+    def perform_create(self, serializer):
+        # path('kitaplar/<int:kitap_pk>/yorum_yap/', api_views.YorumCreateAPIView.as_view(), name='kitap-yorumla'),
+        kitap_pk = self.kwargs.get('kitap_pk')
+        kitap = get_object_or_404(Kitap,pk=kitap_pk)
+        serializer.save(kitap=kitap)
+        
+        
+class YorumDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Yorum.objects.all()
+    serializer_class = YorumSerializers
+    
+    
+    
+    
+    
+    
+    
 
 
 
